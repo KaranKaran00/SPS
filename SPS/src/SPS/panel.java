662d3d569d805b40a9c4de41bd03a6c9a37c3fc0
KaranKaran00 <kharakaran504@gmail.com>
2026-01-28 20:28:35 +0530

    Add files via upload

---
+++ SPS/src/SPS/panel.java
@@ -0,0 +1,78 @@
+package SPS;
+import java.util.Scanner;
+import java.util.Random;
+
+class panel {
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		Random ran=new Random();
+		String[] game= {"STONE","PAPER","SCISSOR"};
+		
+		for(int i=0;i<game.length;i++) {
+			System.out.print(" "+i+":"+game[i]);
+		}
+		System.out.print("\n");
+		
+		int choice=ran.nextInt(game.length);
+		String v1=game[choice];
+		
+		
+		int count1=0;
+		int count2=0;
+		int count3=0;
+		int time=0;
+		
+		while(time<5) {
+			String name = scan.nextLine().trim().toUpperCase();
+			System.out.println("Computer Choice :"+v1);
+		
+		if(name.equals(v1)) 
+		{
+			System.out.println("It TIE! 😯");
+			count1++;
+			//System.out.println();
+			
+		}
+		else if ((name.equals("STONE") && v1.equals("SCISSOR"))||
+				(name.equals("PAPER")  && v1.equals("STONE"))||
+				(name.equals("SCISSOR")&& v1.equals("STONE")))
+		{
+			System.out.println("YOU win 😒");
+			count2++;
+			//System.out.println();
+		}
+				else
+				{
+					System.out.println("YOU LOST ! 😁");
+					count3++;
+					//System.out.println();
+				}
+		time++;
+		}
+		System.out.println();		
+		System.out.println("TIE      : "+count1);
+		System.out.println("You WIN  : "+count2);	
+		System.out.println("You Lost : "+count3);	
+		System.out.println();		
+		if(time == 5)
+		{
+			if(count1 >= 3)
+			{
+				System.out.println("TIE      : "+count1);			
+			}
+			else if(count2 >= 3)
+			{
+				System.out.println("You WIN  : "+count2);			
+			}
+			else if(count3 >=3)
+			{
+				System.out.println("You Lost : "+count3);			
+			}
+			
+			System.out.print("\nGAME END");
+		}
+		
+		scan.close();
+	}
+
+}
